0426fa24ae244a72e3ff267d28c277450c00dfca
knolegsb <seon2you@hotmail.com>
2016-07-01 00:50:17 -0700

    adding admin

---
+++ Mvc5Shopping.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Mvc5Shopping.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5Shopping.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        // GET: Admin
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}
